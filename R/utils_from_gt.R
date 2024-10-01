db67bb1e4c535172b8997357466c02e834638c42
yannsay <yannsay@users.noreply.github.com>
2024-10-01 22:27:02 +0200

    fixing suggests and imports

--- R/utils_from_gt.R
+++ R/utils_from_gt.R
@@ -1,2 +1,10 @@
 glue_gt_gtopenxlsx <- utils::getFromNamespace("glue_gt", "gt")
 resolve_secondary_pattern_gtopenxlsx <- utils::getFromNamespace("resolve_secondary_pattern", "gt")
+
+# unused function to make sure gt does not make an error in
+# Namespace in Imports field not imported from: 'gt'
+# All declared Imports should be used.
+
+not_used <- function() {
+  gt::gt()
+}
