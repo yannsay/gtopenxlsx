e4ea3bbebf4d698c976446e501c8c093f3ae0334
yannsay <yannsay@users.noreply.github.com>
2024-10-03 21:00:42 +0200

    write_source_note migrated to openxlsx2

--- R/write_source_note.R
+++ R/write_source_note.R
@@ -4,21 +4,36 @@
 #'
 #' @return Nothing. It will update the wb object with source note.
 #' @export
-write_source_note <- function(gt_table, ordered_gt_data, wb, sheet_name, row_to_start) {
+write_source_note <- function(wb, row_to_start,
+                              gt_table, ordered_gt_data#,
+                              #sheet_name,
+                              ) {
   current_row <- row_to_start
   total_cols <- ncol(ordered_gt_data)
 
   for (i in 1:length(gt_table$`_source_notes`)) {
-    openxlsx::writeData(wb,
-      sheet_name,
-      x = gt_table[["_source_notes"]][[i]] |> as.character(),
-      startRow = current_row,
-      colNames = F
-    )
+    dims_to_style <- openxlsx2::wb_dims(rows = current_row, cols = 1:total_cols)
 
-    openxlsx::mergeCells(wb, sheet_name, cols = 1:total_cols, rows = current_row)
+    wb <- wb |>
+      openxlsx2::wb_add_data(x = gt_table[["_source_notes"]][[i]] |> as.character(),
+                             start_row = current_row, col_names = FALSE) |>
 
-    openxlsx::addStyle(wb, sheet_name, rows = current_row, cols = 1:total_cols, style = sourcenote_style)
+      # openxlsx::mergeCells(wb, sheet_name, cols = 1:total_cols, rows = current_row)
+      openxlsx2::wb_merge_cells(dims = dims_to_style ) |>
+      openxlsx2::wb_add_font(dims = dims_to_style, size = 10)
+
+
+    # openxlsx::writeData(wb,
+    #   sheet_name,
+    #   x = gt_table[["_source_notes"]][[i]] |> as.character(),
+    #   startRow = current_row,
+    #   colNames = F
+    # )
+
+    # openxlsx::mergeCells(wb, sheet_name, cols = 1:total_cols, rows = current_row)
+
+    # openxlsx::addStyle(wb, sheet_name, rows = current_row, cols = 1:total_cols, style = sourcenote_style)
     current_row <- current_row + 1
   }
+  return(wb)
 }
--- tests/testthat/test-write_source_note.R
+++ tests/testthat/test-write_source_note.R
@@ -1,18 +1,23 @@
 test_that("write_source_note works", {
   temp_dir_to_test <- withr::local_tempdir(fileext = "test")
-  wb <- openxlsx::createWorkbook()
+  # wb <- openxlsx::createWorkbook()
+  wb <- openxlsx2::wb_workbook()
 
-  tester_wrapper <- function(gt_table, sheet) {
-    openxlsx::addWorksheet(wb, sheet)
+
+  tester_wrapper <- function(wb, gt_table, sheet) {
+    wb <- wb |>
+      openxlsx2::wb_add_worksheet(sheet= sheet)
+    # openxlsx::addWorksheet(wb, sheet)
 
     ordered_example <- gt_table |>
       create_ordered_data()
-    write_source_note(gt_table,
-      ordered_example,
+    wb<-write_source_note(gt_table = gt_table,
+        ordered_gt_data = ordered_example,
       wb = wb,
-      sheet_name = sheet,
+      # sheet_name = sheet,
       row_to_start = 1
     )
+    return(wb)
   }
 
   one_source_note <- gtcars_8 |>
@@ -23,27 +28,33 @@ test_that("write_source_note works", {
       )
     )
 
-  tester_wrapper(one_source_note, "one_source_note")
+  wb<-tester_wrapper(wb,one_source_note, "one_source_note")
 
   two_source_note <- one_source_note |>
     gt::tab_source_note(
       source_note = "source note 2"
     )
 
-  tester_wrapper(two_source_note, "two_source_note")
+  wb<-tester_wrapper(wb,two_source_note, "two_source_note")
 
   temp_file_location <- paste0(temp_dir_to_test, "\\footers.xlsx")
-  openxlsx::saveWorkbook(wb, temp_file_location)
+  # openxlsx::saveWorkbook(wb, temp_file_location)
+  openxlsx2::wb_save(wb, temp_file_location)
 
   actual_output <- readxl::excel_sheets(temp_file_location) |>
-    purrr::map(~ openxlsx::read.xlsx(xlsxFile = temp_file_location, sheet = .x)) |>
-    suppressWarnings()
+    # purrr::map(~ openxlsx::read.xlsx(xlsxFile = temp_file_location, sheet = .x)) |>
+    # suppressWarnings()
+    purrr::map(~ openxlsx2::wb_to_df(file = temp_file_location, sheet = .x)) |>
+    suppressMessages()
+
 
   expected_file_location <- testthat::test_path("fixtures", "footers.xlsx")
 
   expected_output <- readxl::excel_sheets(expected_file_location) |>
-    purrr::map(~ openxlsx::read.xlsx(xlsxFile = expected_file_location, sheet = .x)) |>
-    suppressWarnings()
+    # purrr::map(~ openxlsx::read.xlsx(xlsxFile = expected_file_location, sheet = .x)) |>
+    # suppressWarnings()
+    purrr::map(~ openxlsx2::wb_to_df(file = expected_file_location, sheet = .x)) |>
+    suppressMessages()
 
   expect_equal(actual_output[[1]], expected_output[[1]])
   expect_equal(actual_output[[2]], expected_output[[2]])
